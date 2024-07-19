bd85ef05f886979698df2f63c840a2278358a692
Captum <dan.w00tz@gmail.com>
2024-07-19 13:26:05 -0400

    attempting to resolve the branch

--- src/main/java/ui/customevents/PotionSellEvent.java
+++ src/main/java/ui/customevents/PotionSellEvent.java
@@ -1,36 +1,62 @@
 package ui.customevents;
 
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.events.AbstractImageEvent;
 import com.megacrit.cardcrawl.localization.EventStrings;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+
+import potionmod.potions.PotionOption;
 
 public class PotionSellEvent extends AbstractImageEvent {
     public static final String ID = "EventID";
     private static final EventStrings eventStrings = CardCrawlGame.languagePack.getEventString(ID);
     // This text should be set up through loading an event localization json file
     private static final String[] DESCRIPTIONS = eventStrings.DESCRIPTIONS;
-    private static final String[] OPTIONS = eventStrings.OPTIONS;
+    private static PotionOption[] potionOptions;
     private static final String NAME = eventStrings.NAME;
 
     public PotionSellEvent() {
-        super(NAME, DESCRIPTIONS[0], "img/events/eventpicture.png");
+        super(NAME, DESCRIPTIONS[0], "potionmod/images/events/potionvendor.png");
 
-        for (String option : OPTIONS) {
-            this.imageEventText.setDialogOption(option);
+        potionOptions = GenerateDialogOptions();
+        
+        for (PotionOption option : potionOptions) {
+            this.imageEventText.setDialogOption(option.optionText);
         }
     }
 
     @Override
     protected void buttonEffect(int arg0) {
-        if (arg0 == 0) {
-            this.imageEventText.updateBodyText(DESCRIPTIONS[1]);
-            this.imageEventText.updateDialogOption(0, OPTIONS[1]);
-            this.imageEventText.clearRemainingOptions();
-        } else if (arg0 == 1) {
-            this.imageEventText.updateBodyText(DESCRIPTIONS[0]);
-            this.imageEventText.updateDialogOption(0, OPTIONS[0]);
-            this.imageEventText.clearRemainingOptions();
+        //treat the click arg as the index of the potion in the player's potion list
+        //if the player clicks an option, sell the potion and give the player the gold
+        if (arg0 < potionOptions.length - 1 && potionOptions[arg0].potion != null) {
+            PotionOption potion = potionOptions[arg0];
+            AbstractDungeon.topPanel.destroyPotion(potion.potion.slot);
+            potion.potion = null;
+            AbstractDungeon.player.gainGold(potion.sellPrice);
+
+        }
+        else {
+            this.imageEventText.clearAllDialogs();
+            this.imageEventText.setDialogOption("Leave");
+            openMap();
         }
     }
 
+    private PotionOption[] GenerateDialogOptions() {
+        PotionOption[] result = new PotionOption[AbstractDungeon.player.potions.size() - 1];
+
+        for(int i = 0; i < AbstractDungeon.player.potions.size() - 1; i++) {
+            AbstractPotion potion = AbstractDungeon.player.potions.get(i);
+            PotionOption option = new PotionOption(potion, potion.getPrice() / 4);
+            option.optionText = "Sell " + potion.name + " for " + potion.getPrice() / 4 + " gold";
+            result[i] = option;
+        }
+        result[result.length - 1].optionText = "Leave";
+
+        return result;
+    }
 }
+
